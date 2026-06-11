63a2ffc0e5b8dc0bf8811333a34c785e6903f019
hydy100 <62869308+hydy100@users.noreply.github.com>
2026-06-11 18:59:06 +0800

    Update memory pattern in memory.hpp

--- Sakura/memory.hpp
+++ Sakura/memory.hpp
@@ -73,7 +73,7 @@ private:
 		},
 		{
 			{
-				"E8 ? ? ? ? 8B 95 ? ? 00 00 48 8B 0D ? ? ? ? E8 ? ? ? ? 48 8B D8"
+				"E8 ? ? ? ? 48 8D 8D ? ? 00 00 E8 ? ? ? ? 48 85 C0 74 ? 48 85 ED"
 			}, true, false, false, 0, &offsets::functions::FnCharacterDataStack__Push
 		}
 	};
